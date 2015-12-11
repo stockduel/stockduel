3cccc05ef3040bb0e5caaac5103a6d044bd017f8
Anna Rogers <2muddybarefeet@gmail.com>
2015-12-11 15:14:25 -0800

    Add users route and spec file

--- db/schema/matchesSchema.js
+++ db/schema/matchesSchema.js
@@ -1,7 +1,6 @@
 module.exports = function(knex) {
   return knex.schema.createTableIfNotExists('matches', function(table) {
     table.increments('m_id').primary();
-    table.string('title');
     table.integer('creator_id')
         .references('u_id')
         .inTable('users');
@@ -9,9 +8,10 @@ module.exports = function(knex) {
     table.integer('challengee')
         .references('u_id')
         .inTable('users');
-    table.dateTime('startdate');
-    table.dateTime('enddate');
-    table.string('status');
+    //table.dateTime('startdate'); commented out for the time being as not for MVP
+    //table.dateTime('enddate');
+    //table.string('status');
     table.string('type');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
   }).catch(function(e){console.log(e);});
 };
---
+++ server/db/dbcontrollers/users.js
@@ -0,0 +1,71 @@
+var methods = {};
+
+module.exports = function(knex) {
+
+  //-------------------------select all user details-------------------------------//
+
+  methods.getUsers = function() {
+
+    return knex.select().table('users')
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (err) {
+      console.log('error in getting user data',err);
+    });
+
+  };
+
+  //-----------------------get a specific users details---------------------------------//
+
+  methods.getUser = function(userID) {
+    return knex.select().table('users').where('u_id','=', userID)
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (err) {
+      console.log('error in getting user data',err);
+    });
+
+  };
+
+  //-----------------------creates/check a users details----------------------------------//
+
+  methods.createUser = function(username, password, name, email) {
+    //insert user details into the database
+    //check the database for the user
+
+    return knex.select().table('users').where('email','=', email)
+    .then(function (user) {
+      //if the user is already in the table then return the user
+      if (user[0]) {
+        return;
+      } else {
+        //insert the user into the table
+        return knex.insert([
+          {
+          'username': username,
+          'password': password,
+          'name': name,
+          'email': email}], '*')
+        .into('users');
+      }
+    })
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (err) {
+      console.log('error in inserting user data',err);
+    });
+  
+  };
+
+    //-----------------------------------------------------------//
+
+  return methods;
+
+};
+
+
+
+
