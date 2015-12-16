6eccfe20103d641e1e83d52968bb8a3333580bd4
Lucas Ruprecht <lruprecht@gmail.com>
2015-12-16 11:06:53 -0800

    update handling of FB keys for travis

--- server/routes/auth/facebookStrategy.js
+++ server/routes/auth/facebookStrategy.js
@@ -1,5 +1,6 @@
 var TRAVIS = process.env.TRAVIS || false;
 
+//don't use this file if it's travis
 if(!TRAVIS) {
   var FACEBOOK_KEYS = require('./_fb_keys.js');
 }
@@ -7,11 +8,16 @@ var FacebookStrategy = require('passport-facebook');
 
 var usersController = require('../../db/dbcontrollers/usersController');
 
+var CLIENT_ID;
+var CLIENT_SECRET;
+var clientID = TRAVIS ? CLIENT_ID : FACEBOOK_KEYS.CLIENT_ID;
+var clientSecret = TRAVIS ? CLIENT_SECRET : FACEBOOK_KEYS.CLIENT_SECRET;
+
 module.exports = function (knex) {
   var usersCtrl = usersController(knex);
   return new FacebookStrategy({
-      clientID: FACEBOOK_KEYS.CLIENT_ID,
-      clientSecret: FACEBOOK_KEYS.CLIENT_SECRET,
+      clientID: clientID,
+      clientSecret: clientSecret,
       callbackURL: "http://localhost:8080/auth/facebook/callback",
       enableProof: false,
       profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
