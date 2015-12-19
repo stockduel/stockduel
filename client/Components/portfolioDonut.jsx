5131cc78565254006d2caea1ced61a54414b0d95
tatethurston <tatethurston@gmail.com>
2015-12-19 15:01:10 -0800

    add portfolio donut component

---
+++ client/Components/portfolioDonut.jsx
@@ -0,0 +1,35 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import c3 from 'c3';
+
+class PortfolioDonut extends React.Component {
+  
+  constructor() {
+    super();
+  }
+
+  componentDidMount(){
+    this.buildChart(this.props.stocks);
+  }
+
+  buildChart(stocks) {
+    c3.generate({
+      bindto: ReactDOM.findDOMNode(this.refs.chart),
+      data: 
+          {json : stocks,
+          type : 'donut'        
+      },
+      donut: {
+          title: "Portfolio"
+      }
+    });
+  }
+  
+  render(){
+    return (
+    <div ref="chart"></div>
+    )
+  }
+};
