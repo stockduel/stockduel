c3522a807552dc2b6171ec4f542d892bf9d619ce
Lucas Ruprecht <lruprecht@gmail.com>
2015-12-30 12:11:42 -0800

    update match workers to compare proper date format with db

--- workers/processMatches/startMatches/startMatches.js
+++ workers/processMatches/startMatches/startMatches.js
@@ -9,10 +9,12 @@ module.exports = function (knex) {
 //Seached for any matches that start today and update status depending on if it has a chalangee or not
 //------------------------------------------------------------------------------------------------------
   module.startMatches = function (providedDate) {
+    console.log('kicking off match starting job');
     //formulate date to look for
     var today = new Date();
-    var date = providedDate || new Date(today.getFullYear(), today.getMonth(), today.getDate(), 14);
       //get all matches that should start today
+    var date = providedDate || new Date(today.getFullYear(), today.getMonth(), today.getDate(), 14, 30);
+    //get all matches that should start today
       module.selectPendingMatches(date)
       .then( function(matches){
         return module.updateMatchState(matches);
@@ -24,15 +26,19 @@ module.exports = function (knex) {
 
 //Select all pending matches that start on passed in date (when used this is looking for all matches starting today)
   module.selectPendingMatches = function (date) {
+    console.log('finding pending matches');
     return knex('matches')
-      .where({
-        status: 'pending',
-        startdate: date
+      .where('status', 'pending')
+      .then( function(matches) {
+        return matches.filter(function(match) {
+          return Date.parse(match.startdate) <= Date.parse(date);
+        });
       });
   };
 
 //Decide if a match can become active or not depending on whether it has a chalengee
   module.updateMatchState = function (matches) {
+    console.log('updating match states');
     return Promise.map(matches, function (match) {
       if( match.challengee ) {
         return module.activateMatch(match);
@@ -50,6 +56,7 @@ module.exports = function (knex) {
       .andWhereNot({challengee: null})
       .update({status: 'active'}, '*')
       .then( function (match) {
+        console.log('activating match');
         return match[0];
       })
       .catch( function (err) {
@@ -65,6 +72,7 @@ module.exports = function (knex) {
       .andWhere({challengee : null})
       .update({status: 'rejected'}, '*')
       .then( function (rejectedMatch) {
+        console.log('rejecting match');
         return rejectedMatch[0];
       })
       .catch( function (err) {
