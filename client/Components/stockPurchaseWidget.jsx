f4989ec429888492d26df512b932deee36589fdf
CJ Apel <cjapel@CJs-MacBook-Air.local>
2015-12-29 17:44:27 -0800

    refactor searchStocks with stockPurchaseWidget

--- client/Components/stockPurchaseWidget.jsx
+++ client/Components/stockPurchaseWidget.jsx
@@ -11,17 +11,10 @@ const TextField = require('material-ui/lib/text-field');
 export const StockPurchase = React.createClass({
 
   render() {
-    const { buy, MatchId, userId, valueProp, MatchTitle, isActive } = this.props;
-    let stockTicker = valueProp;
+    const { buy, MatchId, userId, stockTicker } = this.props;
     let numShares;
     return (
-      <div className="cardMarginBottom paddingTop centreTitle">
-      <h6>{MatchTitle + (isActive ? '' : ' is not currently active')}</h6>
-      {isActive && <div className="purchaseContainer">
-         <TextField hintText="Stock Symbol" ref="symbolInput" onChange={ function () {
-          stockTicker = arguments[0].target.value;
-         }} value={valueProp} />
-
+      <div className="purchaseContainer">
          <TextField hintText="Number of Stocks" ref="numSharesInput" onChange={ function () {
           numShares = arguments[0].target.value;
          }} />
@@ -36,14 +29,11 @@ export const StockPurchase = React.createClass({
               userId: userId,
               action: 'buy'
             };
-            this.refs.symbolInput.refs.input.value = "";
             this.refs.numSharesInput.refs.input.value = "";
             buy(buyOptions);
             window.location.hash="#/portfolio";
           }
         }/>
-
-      </div>}
 
       </div>
     );
