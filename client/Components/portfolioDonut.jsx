e124905b9f2160e6439c0092deea5748327c5900
Anna Rogers <2muddybarefeet@gmail.com>
2015-12-29 17:11:09 -0800

    Add in Tate's donut to the portfolio view

--- client/Components/portfolioDonut.jsx
+++ client/Components/portfolioDonut.jsx
@@ -1,36 +1,34 @@
-
 'use strict';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { toJS } from 'immutable';
 import c3 from 'c3';
 
-class PortfolioDonut extends React.Component {
-  
-  constructor() {
-    super();
-  }
+export const PortfolioDonut = React.createClass ({
 
   componentDidMount(){
+    console.log('there---->',this.props.stocks);
     this.buildChart(this.props.stocks);
-  }
+  },
 
   buildChart(stocks) {
     c3.generate({
       bindto: ReactDOM.findDOMNode(this.refs.chart),
-      data: 
-          {json : stocks,
-          type : 'donut'        
+      data: {
+        json : stocks,
+        type : 'donut'        
       },
       donut: {
-          title: "Portfolio"
+        title: "Portfolio"
       }
     });
-  }
+  },
   
   render(){
     return (
       <div ref="chart"></div>
     )
   }
-};
+
+});
