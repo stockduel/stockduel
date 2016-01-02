5f041d355601ed48cdca276facf5f5bf2803e407
Anna Rogers <2muddybarefeet@gmail.com>
2016-01-02 15:01:41 -0800

    Add text and button to show if you have not yet made a  match

--- client/Components/matches.jsx
+++ client/Components/matches.jsx
@@ -53,9 +53,16 @@ const MatchesList = React.createClass({
   //       return "Solo Match. Start date: " + matchObj.get('startdate').substr(0, 10);
   //     }
   // },
-
+//
   render() {
     const { matches, userId, createMatch } = this.props;
+    console.log('matches', matches.toJS())
+
+    let noMatches;
+    if ( (matches.count() ===1 && matches.get('m_id') === "") || matches.count() === 0) {
+      console.log('none ^^^^');
+      noMatches = (<div className="centreTitle headerPaddingTop" ><h3>You have no matches yet</h3><RaisedButton label="Create Match" link={true} href='/#/create' /></div>);
+    }
 
     return (
       <div>
@@ -69,6 +76,8 @@ const MatchesList = React.createClass({
         {/*tab one current matches*/}
           <Tab style={{background:'white', fontSize:'15px', color: "#484848"}} label="Current Matches" value="a" >
             <div className="container">
+
+            {noMatches}
 
               <ul>
                 {matches.map((match,index) => { 
@@ -86,6 +95,8 @@ const MatchesList = React.createClass({
           <Tab style={{background:'white', fontSize:'15px', color: "#484848"}} label="Pending Matches" value="b">
             <div className="container">
               
+              {noMatches}
+
               <ul>
                 {matches.map( (match,index) => {
                   if (match.get('status') === "pending") {
@@ -100,6 +111,8 @@ const MatchesList = React.createClass({
         {/*tab three complete matches*/}
           <Tab style={{background:'white', fontSize:'15px', color: "#484848"}} label="Past Matches" value="c">
             <div className="container">
+
+              {noMatches}
 
               <ul>
                 {matches.map((match,index) => {
